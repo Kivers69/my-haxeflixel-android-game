96afbd0d9a5aa3c804f0b878c4996563bdcca6e6
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-11-04 15:12:57 +0800

    Add files via upload

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -8,12 +8,12 @@ class WarningState extends FlxState
 
     override public function create()
     {
-        super.create();
-
         var menuTxt = new flixel.text.FlxText(0, 0, 0, menuText, 64);
         menuTxt.screenCenter();
         menuTxt.size = 30;
         add(menuTxt);
+
+        super.create();
     }
 
     override public function update(elapsed:Float)
--- source/WarningState.hx
+++ source/WarningState.hx
@@ -9,21 +9,20 @@ class WarningState extends FlxState
 
     override public function create()
     {
-        super.create();
-
         var warnTxt = new flixel.text.FlxText(0, 0, 0, warnText, 64);
         warnTxt.screenCenter();
         warnTxt.size = 30;
         add(warnTxt);
+
+        super.create();
     }
 
     override public function update(elapsed:Float)
     {
-        super.update(elapsed);
-
         if (FlxG.mouse.justPressed)
         {
-            FlxG.switchState(new menuState());
+            FlxG.switchState(new MenuState());
         }
+        super.update(elapsed);
     }
 }
