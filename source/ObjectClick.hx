2ca45bca262227e027a31664dede1f5826940d92
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-11-05 16:24:24 +0800

    Add files via upload

--- source/ObjectClick.hx
+++ source/ObjectClick.hx
@@ -5,19 +5,19 @@ import flixel.FlxBasic;
 
 class ObjectClick extends FlxG
 {
-    public static function clickPressed(ObjectOrGroup:FlxBasic):Void
+    public static function clickPressed(?ObjectOrGroup:FlxBasic):Void
     {
-        if (FlxG.mouse.overlaps(ObjectOrGroup) && flxG.mouse.pressed)
+        if (FlxG.mouse.overlaps(ObjectOrGroup) && FlxG.mouse.pressed)
         {}
     }
     public static function clickJustPressed(?ObjectOrGroup:FlxBasic):Void
     {
-        if (FlxG.mouse.overlaps(ObjectOrGroup) && flxG.mouse.justPressed)
+        if (FlxG.mouse.overlaps(ObjectOrGroup) && FlxG.mouse.justPressed)
         {}
     }
     public static function clickJustReleased(?ObjectOrGroup:FlxBasic):Void
     {
-        if (FlxG.mouse.overlaps(ObjectOrGroup) && flxG.mouse.justReleased)
+        if (FlxG.mouse.overlaps(ObjectOrGroup) && FlxG.mouse.justReleased)
         {}
     }
 }
