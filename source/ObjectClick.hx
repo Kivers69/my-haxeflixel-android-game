ca87c595a61613649d4924ee566c021aaf9c68ac
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-11-05 16:42:34 +0800

    Pls work

--- source/ObjectClick.hx
+++ source/ObjectClick.hx
@@ -2,20 +2,21 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxBasic;
+import flixel.FlxState;
 
-class ObjectClick extends FlxG
+class ObjectClick extends FlxState
 {
-    public static function clickPressed(?ObjectOrGroup:FlxBasic):Void
+    public static function clickPressed(?ObjectOrGroup:FlxBasic)
     {
         if (FlxG.mouse.overlaps(ObjectOrGroup) && FlxG.mouse.pressed)
         {}
     }
-    public static function clickJustPressed(?ObjectOrGroup:FlxBasic):Void
+    public static function clickJustPressed(?ObjectOrGroup:FlxBasic)
     {
         if (FlxG.mouse.overlaps(ObjectOrGroup) && FlxG.mouse.justPressed)
         {}
     }
-    public static function clickJustReleased(?ObjectOrGroup:FlxBasic):Void
+    public static function clickJustReleased(?ObjectOrGroup:FlxBasic)
     {
         if (FlxG.mouse.overlaps(ObjectOrGroup) && FlxG.mouse.justReleased)
         {}
