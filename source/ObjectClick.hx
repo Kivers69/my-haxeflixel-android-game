f699d79c6eba166aa6d3e56f86b44b9d891577c9
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-11-05 15:15:05 +0800

    Add files via upload

--- source/ObjectClick.hx
+++ source/ObjectClick.hx
@@ -4,14 +4,12 @@ import flixel.FlxG;
 
 class ObjectClick extends FlxG
 {
-    public static function click(obj:String):Bool
+    public static function click(object:String):Void
     {
-        if (FlxG.mouse.x > obj.x &&
-            FlxG.mouse.x < obj.x + obj.width &&
-            FlxG.mouse.y > obj.y &&
-            FlxG.mouse.y < obj.y + obj.height)
-        {
-            return true;
-        }
+        if (FlxG.mouse.x > object.x &&
+            FlxG.mouse.x < object.x + object.width &&
+            FlxG.mouse.y > object.y &&
+            FlxG.mouse.y < object.y + object.height)
+        {}
     }
 }
