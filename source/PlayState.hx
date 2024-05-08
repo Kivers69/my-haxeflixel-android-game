15a8cdf30ad29632d1e54e5e7fecdce375c97a66
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-09 00:57:19 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,14 +1,11 @@
 package;
 
 import flixel.FlxG;
-import flixel.FlxState;
 import flixel.FlxSprite;
 import flixel.text.FlxText;
 
-class PlayState extends FlxState
-{
-    override public function create()
-    {
+class PlayState extends GameState {
+    override public function create() {
         var textThing:String = 'no title for this game :(';
         var notdone:FlxText = new FlxText(0, 0, FlxG.width,textThing, 20);
         notdone.alignment = FlxTextAlign.CENTER;
@@ -18,11 +15,10 @@ class PlayState extends FlxState
         super.create();
     }
 
-    override public function update(elapsed:Float)
-    {
+    override public function update(elapsed:Float) {
         super.update(elapsed);
         if (FlxG.mouse.justPressed) {
-            //FlxG.switchState(new gameState);
+            //switchState(new gameState);
         }
     }
 }
