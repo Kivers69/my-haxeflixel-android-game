1980ba6f95a025e9094f96210cd7a839ada2ba78
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-09 00:49:04 +0800

    remove function new

--- source/GameState.hx
+++ source/GameState.hx
@@ -1,9 +1,5 @@
 import FlxState;
 class GameState extends FlxState {
-  new function() {
-    super();
-  }
-
   public static function switchState(nextState:FlxState) {
       FlxG.switchState(nextState);
   }
