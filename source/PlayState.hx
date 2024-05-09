bd38cbd090497c2ebdc4ef11b38afc0d2652bec2
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-09 15:15:35 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,7 +9,6 @@ class PlayState extends GameState {
         var textThing:String = 'no title for this game :(';
 
         var notdone:FlxText = new FlxText(0, 0, FlxG.width,textThing, 20);
-        notdone.scrollfactor.set();
         add(notdone);
 
         var tileThing:Tiles = new Tiles('room');
