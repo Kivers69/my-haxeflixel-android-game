56dcdf13160bb20ab1fbeb2b72edbc982daa2461
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-09 14:59:39 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,10 +7,13 @@ import flixel.text.FlxText;
 class PlayState extends GameState {
     override public function create() {
         var textThing:String = 'no title for this game :(';
+
         var notdone:FlxText = new FlxText(0, 0, FlxG.width,textThing, 20);
-        notdone.alignment = FlxTextAlign.CENTER;
-        notdone.screenCenter();
+        notdone.scrollfactor.set();
         add(notdone);
+
+        var tileThing:Tiles = new Tiles('room');
+        add(tileThing);
 
         super.create();
     }
