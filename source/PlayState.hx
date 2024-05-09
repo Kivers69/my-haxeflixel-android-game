94fdb9c3f546cc0737c824b1c0d956788776e07b
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-10 05:45:48 +0800

    test

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,12 +4,14 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.text.FlxText;
 import sys.FileSystem;
+import tea.SScript;
 
 using StringTools;
 
 class PlayState extends GameState {
     var textThing:String = 'no title for this game';
     var notdone:FlxText;
+    var hscripts:SScript;
     override function create() {
         notdone = new FlxText(0, 0, FlxG.width,'', 20);
         add(notdone);
@@ -17,14 +19,18 @@ class PlayState extends GameState {
         //testing
         for (file in FileSystem.readDirectory(SUtil.getPath() + 'assets/scripts')) {
             if (file.endsWith('.hx')) {
+                var path:String = SUtil.getPath() + 'assets/scripts/' + file;
+                hscripts = new SScript(path);
+                hscripts.execute();
                 notdone.text = textThing + '\nscripts file list\n' + file;
             }
         }
-
         super.create();
+        hscripts.call('create');
     }
 
     override function update(elapsed:Float) {
         super.update(elapsed);
+        hscripts.call('update');
     }
 }
