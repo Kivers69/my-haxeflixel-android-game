c19dea35fb5cbbaafc11b1de138389bc8d234a1d
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-09 18:55:32 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -11,8 +11,10 @@ class PlayState extends GameState {
         var notdone:FlxText = new FlxText(0, 0, FlxG.width,textThing, 20);
         add(notdone);
 
+        /*
         var tileThing:Tiles = new Tiles(0,0,'room');
         add(tileThing);
+        */
 
         super.create();
     }
