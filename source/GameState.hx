2a420eb9853fb0ccff9c259e92e8e5ee8ad6d5c8
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-09 10:49:34 +0800

    Update GameState.hx

--- source/GameState.hx
+++ source/GameState.hx
@@ -1,4 +1,8 @@
+package;
+
 import flixel.FlxState;
+import flixel.FlxG;
+
 class GameState extends FlxState {
     public static function switchState(nextState:FlxState) {
         FlxG.switchState(nextState);
