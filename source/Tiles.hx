5a9326e53a474fdeeeb455c1b8c26f58e0aa78f8
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-09 15:56:16 +0800

    Update Tiles.hx

--- source/Tiles.hx
+++ source/Tiles.hx
@@ -15,7 +15,7 @@ typedef SolidArray = {
     var y:Float;
 }
 
-class Tiles extends FlxObject {
+class Tiles extends FlxSprite {
     public function new(x:Float, y:Float, ?json:String = 'room') {
         super(x, y);
 
