0678682c39829f5b57ffeeb778a43314107d4b90
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-10 00:31:37 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,13 +5,22 @@ import flixel.FlxSprite;
 import flixel.text.FlxText;
 import sys.FileSystem;
 
+using StringTools;
+
 class PlayState extends GameState {
     var textThing:String = 'no title for this game';
     var notdone:FlxText;
     var scripts:SScript;
     override function create() {
-        notdone = new FlxText(0, 0, FlxG.width,textThing, 20);
+        notdone = new FlxText(0, 0, FlxG.width,'', 20);
         add(notdone);
+
+        //testing
+        for (file in FileSystem.readDirectory('assets/scripts')) {
+            if (file.endsWith('.hx')) {
+                notdone.text = textThing + '\nscripts file list' + file + '\n';
+            }
+        }
 
         super.create();
     }
