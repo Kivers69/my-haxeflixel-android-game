3e657144ead4c6ad3e3788a4e4e7751ae3890df1
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-10 08:30:13 +0800

    test

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,19 +10,24 @@ using StringTools;
 
 class PlayState extends GameState {
     var textThing:String = 'no title for this game';
-    var notdone:FlxText;
+    var path:String;
+    var texts:FlxText;
+    var fileList:FlxText;
     var hscripts:SScript;
     override function create() {
-        notdone = new FlxText(0, 0, FlxG.width,'', 20);
-        add(notdone);
+        texts = new FlxText(0, 0, FlxG.width, 'textThing', 20);
+        add(texts);
+        
+        fileList = new FlxText(0, 25, FlxG.width, 'hscript file list\n', 20);
+        add(fileList);
 
         //testing
         for (file in FileSystem.readDirectory(SUtil.getPath() + 'assets/scripts')) {
             if (file.endsWith('.hx')) {
-                var path:String = SUtil.getPath() + 'assets/scripts/' + file;
+                path += SUtil.getPath() + 'assets/scripts/' + file;
                 hscripts = new SScript(path);
                 hscripts.execute();
-                notdone.text = textThing + '\nscripts file list\n' + file;
+                fileList.text += file + '\n';
             }
         }
         super.create();
