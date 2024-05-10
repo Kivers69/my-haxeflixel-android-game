d97bfbf482ae6b71962e448f8e45e1e0ebb80eeb
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-10 09:48:14 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -24,16 +24,17 @@ class PlayState extends GameState {
         //testing
         for (file in FileSystem.readDirectory(SUtil.getPath() + 'assets/scripts')) {
             if (file.endsWith('.hx')) {
-                if (file.length < 0 )
+                if (file.length < 0 ) {
                     path += SUtil.getPath() + 'assets/scripts/' + file;
                     hscripts = new SScript(path);
                     hscripts.execute();
                     fileList.text += file + '\n';
                 } else {
-                    fileList.text += 'no hx file\n';
+                    fileList.text += 'no hx file found\n';
                 }
             }
         }
+        
         super.create();
         hscripts.call('create');
     }
