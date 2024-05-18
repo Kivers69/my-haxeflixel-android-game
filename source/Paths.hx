86aed782e355010d6bbf2a44fb8b72266044b464
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-18 23:20:50 +0800

    am i stupid

--- source/Paths.hx
+++ source/Paths.hx
@@ -2,10 +2,10 @@ package;
 
 class Paths {
     static public function getPath(file:String) {
-        return SUtil.getPath() + 'assets/' + file;
+        return SUtil.getPath() + 'assets/$file';
     }
 
     inline static public function image(key:String):Dynamic {
-        return getPath('images/' + key + '.png');
+        return getPath('images/$key.png');
     }
 }
