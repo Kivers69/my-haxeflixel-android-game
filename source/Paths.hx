7d77dec8284e078b751a17d4aa167d286df1258c
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-18 21:49:11 +0800

    inage still not loading

--- source/Paths.hx
+++ source/Paths.hx
@@ -2,10 +2,10 @@ package;
 
 class Paths {
     static public function getPath(file:String) {
-        return SUtil.getPath() + 'assets/$file';
+        return SUtil.getPath() + 'assets/' + file;
     }
 
-    inline static public function image(key:String):String {
-        return getPath('images/$key.png');
+    inline static public function image(key:String) {
+        return getPath('images/' + key + '.png');
     }
 }
