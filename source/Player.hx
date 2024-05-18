97c638dca96746d247ba8ff049b8b72dd59423c0
kivers <123798197+Kivers69@users.noreply.github.com>
2024-05-18 19:18:13 +0800

    j

--- source/Player.hx
+++ source/Player.hx
@@ -4,10 +4,10 @@ import flixel.FlxSprite;
 import flixel.FlxG;
 
 class Player extends FlxSprite {
-    public function new(x:Float, y:Float, char:String, w:Int, h:Int, ?facings:String = 'down') {
-        super();
+    public function new(x:Float, y:Float, ?image:String = 'frisk', w:Int, h:Int, ?facings:String = 'down') {
+        super(x, y);
 
-        loadGraphic(Paths.image(char), false, w, h);
+        loadGraphic(Paths.image(image), false, w, h);
         animation.add('left', [0,1,0,1], 5, false);
         animation.add('right', [2,3,3,3], 5, false);
         animation.add('up', [4,5,4,5], 5, false);
